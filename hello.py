5b0f16fb10688fdabb507cc9525376020fbaf69b
omarlari@gmail.com <olari@784f435df7a4.ant.amazon.com>
2017-11-13 14:36:54 -0800

    demo

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello, ECS Summit!!!'
+    return 'Hello, Demo123!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
